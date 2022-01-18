b5fe480c34e523cbafdf901c302ed02849614963
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-01-18 15:36:27 +0100

    api

---
+++ Controllers/BookApiController.cs
@@ -0,0 +1,104 @@
+﻿using eLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eLibrary.Controllers
+{
+    [Route("api/context")]
+    [ApiController]
+    public class BookApiController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public BookApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetCar()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetCar(int id)
+        {
+            var car = await _context.Books.FindAsync(id);
+
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return car;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCar(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBook(Book book)
+        {
+            _context.Books.Add(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCar", new { id =book.Id }, book);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            var car = await _context.Books.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            _context.Books.Remove(car);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.Id == id);
+        }
+    }
+}
+
